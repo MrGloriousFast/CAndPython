3f07da11049be88fd5430ce99d733b8418cb506a
MrGLoriousFast <schirmcharmemelone@gmail.com>
2017-06-13 02:41:45 +0200

    made a super optmized version xD

---
+++ direct.py
@@ -0,0 +1,21 @@
+import time, sys
+
+#the test function
+#you can copy this file and implement another way to get the same result
+def run(x):
+    if (x < 1): x =1
+    
+    return x
+
+
+#dont touch this
+#measure code and output that we need to compare implementations
+x = 3000 #int(sys.argv[1])
+step = 100
+for xx in range(0 , x+step, step):
+    now = time.process_time()
+    result = run(xx)
+    print (xx , ",", time.process_time() - now)
+    
+#compare the result to other implementations to knwo your code runs correct
+print (x , ",", time.process_time(), ", total", result)
